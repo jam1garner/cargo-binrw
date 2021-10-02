94eda83792c6f72f7d80e23fe6d006eee2616975
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-10-02 01:43:27 -0400

    Fix installation as a cargo subcommand

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,19 @@ pub struct Args {
     // TODO
 }
 
+#[cfg(feature = "cli")]
+#[derive(StructOpt)]
+#[structopt(bin_name = "cargo")]
+pub enum CargoArgsWrapper {
+    Binrw(Args),
+}
+
 pub fn main(args: Args) {
     todo!()
 }
 
 #[cfg(feature = "cli")]
 pub fn main_from_args() {
-    main(Args::from_args())
+    let CargoArgsWrapper::Binrw(args) = StructOpt::from_args();
+    main(args)
 }

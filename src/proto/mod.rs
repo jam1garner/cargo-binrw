d9f0378e48674dd0cfad88723bb62544eaa964fc
David M. Golembiowski <david@dgolembiowski.com>
2021-12-07 23:22:54 -0500

    Update mod.rs
    Fixing the unnecessary crate sugar

--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -11,14 +11,14 @@ use binrw::{BinRead, BinWrite};
 /// then the data pertaining to that message.
 #[derive(Clone, PartialEq, Default)]
 pub(crate) struct Call<'a, T: BinrwMessage + Sized> (
-    pub(crate) /* msg_count: */ u16,
-    pub(crate) /* messages:  */ Cow<&'a T>,
+    pub /* msg_count: */ u16,
+    pub /* messages:  */ Cow<&'a T>,
 );
 
 #[derive(Clone, PartialEq, Default)]
 pub(crate) struct BinrwMessage<T: BinRead + BinWrite + Sized> (
-    pub(crate) /* len:  */ u16,
-    pub(crate) /* data: */ T,
+    pub /* len:  */ u16,
+    pub /* data: */ T,
 );
 
 impl From<T: BinRead + Binwrite + Sized> for BinrwMessage {

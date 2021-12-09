1113c8541cfb5ef7b722fd46aae96e4436471c5c
David Golembiowski <david@dgolembiowski.com>
2021-12-08 21:57:10 -0500

    `cargo build` fails since it finds a virtual manifest instead of a package manifest

--- binrw-tracing/src/lib.rs
+++ binrw-tracing/src/lib.rs
@@ -1,3 +1,16 @@
+#![warn(
+    missing_debug_implementations,
+    missing_docs,
+    rust_2018_idioms,
+    unreachable_pub
+)]
+#![doc(test(
+    no_crate_inject,
+    attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
+))]
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]

0940d76e04593a45df80a62145bf0599572caf94
David Golembiowski <david@dgolembiowski.com>
2021-12-15 22:47:51 -0500

    Tests are in place finally

--- cargo-binrw/tests/cli.rs
+++ cargo-binrw/tests/cli.rs
@@ -3,7 +3,8 @@ use assert_cmd::assert::OutputAssertExt;
 use assert_cmd::cargo::CommandCargoExt;
 use assert_cmd::output::OutputOkExt;
 use fixtures::Error;
-use std::process::Command;
+use std::process::{ Command, Stdio };
+use core::time::Duration;
 use structopt::clap::{crate_name, crate_version};
 
 fn cargo_binrw() -> Command {
@@ -14,15 +15,40 @@ fn cargo_binrw() -> Command {
 fn help() {
     cargo_binrw().args(&["binrw", "help"]).assert().success();
 }
-// Commenting out this test for now because it breaks things
-/*#[test]
+
+#[test]
 fn run() {
-    cargo_binrw().args(&["binrw", "run"]).assert().success();
-}*/
+    use wait_timeout::ChildExt;
+   
+
+    let mut child = cargo_binrw()
+        .args(&["binrw", "run"])
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    
+    let secs = Duration::from_secs(2);
+    let _status_code = match child.wait_timeout(secs).unwrap() {
+        Some(status) => status.code(),
+        None => {
+            child.kill().unwrap();
+            child.wait().unwrap().code()
+        }   
+    };
+}
+
 #[test]
 fn new() {
-    cargo_binrw().args(&["binrw", "new"]).assert().success();
+    cargo_binrw().args(&["binrw", "new", "."]).assert().success();
 }
+
+#[test]
+fn new_fail() {
+    cargo_binrw().args(&["binrw", "new"]).assert().failure();
+}
+
+
+
 #[test]
 fn fuzz() {
     cargo_binrw().args(&["binrw", "fuzz"]).assert().success();
